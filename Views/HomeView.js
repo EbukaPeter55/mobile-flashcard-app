564cc6dc5820e6f0076a8896f1cb53a667bf64a2
ebuka <peterdbrainy5@gmail.com>
2021-12-18 23:08:17 +0100

    Start coding the UI for HomeView and install some dependencies like reactredux, navigation and stacks

---
+++ Views/HomeView.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, Stylesheet } from "react-native";
+import { Button } from "react-native-paper";
+
+
+
+
+const HomeView = () => {
+    <View style={styles.container}>
+        <View style={styles.container}> 
+            <View>
+                <Button>
+
+                </Button>
+            </View>
+        </View>
+
+    </View>
+
+
+}
+
+
+
+export default HomeView;
+
+const styles = Stylesheet.create({
+    container: {
+        flex: 1
+    }
+})
